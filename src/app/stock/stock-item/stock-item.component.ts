f8b3c7c2a2f1a45c8f5e682a6beefd4f1715c475
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-27 06:37:45 +0100

    Add NgFor directive to render more instances

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -7,16 +7,33 @@ import { Stock } from 'src/app/model/stock';
   styleUrls: ['./stock-item.component.css'],
 })
 export class StockItemComponent implements OnInit {
-  stock!: Stock;
+  stocks!: Stock[];
+  stocksClasses!: any[];
 
   constructor() {}
 
   ngOnInit(): void {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 85, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765),
+    ];
+    let diffs = this.stocks.map(
+      (stock) => stock.price / stock.previousPrice - 1
+    );
+    let largeChanges = diffs.map((diff) => Math.abs(diff) > 0.01);
+    this.stocksClasses = this.stocks.map((stock, i) => {
+      return {
+        positive: stock.isPositiveChange(),
+        negative: !stock.isPositiveChange(),
+        'large-change': largeChanges[i],
+        'small-change': !largeChanges[i],
+      };
+    });
   }
 
-  toggleFavourite(event: MouseEvent) {
+  toggleFavourite(event: MouseEvent, i: number) {
     console.log('We are toggling the favourite state for this stck', event);
-    this.stock.favourite = !this.stock.favourite;
+    this.stocks[i].favourite = !this.stocks[i].favourite;
   }
 }
